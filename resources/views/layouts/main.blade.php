2c109dda3645f9e3435f175996553d130689202c
FreekAlberti <federicoalberi96@gmail.com>
2020-10-20 17:53:09 +0200

    header

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,16 @@
   <title>@yield('titolo-pagina')</title>
 </head>
 <body>
+    <header>
+        <div class="box-flex-header">
+            <img class="logo" src="{{asset("img/marchio-sito-test.png")}}" alt="logo">
+            <ul class="nav-header">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Prodotti</a></li>
+                <li><a href="#">News</a></li>
+            </ul>
+        <div/>
+    </header>
     @yield('main-content')
 </body>
 </html>
