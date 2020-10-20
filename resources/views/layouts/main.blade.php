e86a33527c2fb836230b28db743ced83eac8a928
FreekAlberti <federicoalberi96@gmail.com>
2020-10-20 18:02:41 +0200

    include

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,16 +8,7 @@
   <title>@yield('titolo-pagina')</title>
 </head>
 <body>
-    <header>
-        <div class="box-flex-header">
-            <img class="logo" src="{{asset("img/marchio-sito-test.png")}}" alt="logo">
-            <ul class="nav-header">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Prodotti</a></li>
-                <li><a href="#">News</a></li>
-            </ul>
-        <div/>
-    </header>
+    @include('partials/header');
     @yield('main-content')
 </body>
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <div class="box-flex-header">
+        <img class="logo" src="{{asset("img/marchio-sito-test.png")}}" alt="logo">
+        <ul class="nav-header">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Prodotti</a></li>
+            <li><a href="#">News</a></li>
+        </ul>
+    <div/>
+</header>
