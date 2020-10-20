dc1ebfa11e4111ce7f4ca4b3df7021877780825b
FreekAlberti <federicoalberi96@gmail.com>
2020-10-20 16:58:23 +0200

    layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,47 +16,42 @@
   }
 @endphp
 
+@extends('layouts.main');
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="{{asset("css/app.css")}}">
-  <title>Document</title>
-</head>
-<body>
-  <section>
-    @if (count($lunga) > 0)    
-      <h2>Lunga</h2>
-      @foreach ($lunga as $item)
-      <div class="box-pasta">
-        <img src="{{$item["src"]}}" alt="">
-        <h4>{{$item["titolo"]}}</h4>    
-      </div>
-      @endforeach
-    @endif
+@section('titolo-pagina')
+  Home
+@endsection
 
-    @if (count($corta) > 0)        
-      <h2>Corta</h2>
-      @foreach ($corta as $item)
-      <div class="box-pasta">
-        <img src="{{$item["src"]}}" alt="">
-        <h4>{{$item["titolo"]}}</h4>    
-      </div>
-      @endforeach
-    @endif
+@section('main-content')
+<section>
+  @if (count($lunga) > 0)    
+    <h2>Lunga</h2>
+    @foreach ($lunga as $item)
+    <div class="box-pasta">
+      <img src="{{$item["src"]}}" alt="">
+      <h4>{{$item["titolo"]}}</h4>    
+    </div>
+    @endforeach
+  @endif
 
-    @if (count($cortissima) > 0)    
-      <h2>Cortissima</h2>
-      @foreach ($cortissima as $item)
-      <div class="box-pasta">
-        <img src="{{$item["src"]}}" alt="">
-        <h4>{{$item["titolo"]}}</h4>    
-      </div>
-      @endforeach
-    @endif
-  </section>
-</body>
-</html>
+  @if (count($corta) > 0)        
+    <h2>Corta</h2>
+    @foreach ($corta as $item)
+    <div class="box-pasta">
+      <img src="{{$item["src"]}}" alt="">
+      <h4>{{$item["titolo"]}}</h4>    
+    </div>
+    @endforeach
+  @endif
+
+  @if (count($cortissima) > 0)    
+    <h2>Cortissima</h2>
+    @foreach ($cortissima as $item)
+    <div class="box-pasta">
+      <img src="{{$item["src"]}}" alt="">
+      <h4>{{$item["titolo"]}}</h4>    
+    </div>
+    @endforeach
+  @endif
+</section>
+@endsection
