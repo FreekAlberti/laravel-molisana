d1f5aca6a7bb71fb061d899c63daf3281405c15c
FreekAlberti <federicoalberi96@gmail.com>
2020-10-24 17:59:44 +0200

    product style

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -5,9 +5,14 @@
 @endsection
 
 @section('main-content')
-    {{-- @dd($product); --}}
-    <h1>{{$product["titolo"]}}</h1>
+<div class="product">
+  <div class="container">
+    <div class="box-flex">
+      <h1>{{$product["titolo"]}}</h1>
+    </div>
     <img src="{{$product["src-h"]}}" alt="">
     <img src="{{$product["src-p"]}}" alt="">
-    <p>{!!$product["descrizione"]!!}</p>
+    <div class="descrizione-prodotto">{!!$product["descrizione"]!!}</div>
+  </div>
+</div>
 @endsection
