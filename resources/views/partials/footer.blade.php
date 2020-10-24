0308280d95d7eb8eebe918d9e1aab3e05c492167
FreekAlberti <federicoalberi96@gmail.com>
2020-10-24 15:55:32 +0200

    footer style

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,19 +1,19 @@
 <footer>
     <div class="container wrapper">
-        <div class="footer-left">
+        <div class="footer-left padding-footer">
             <img class="logo" src="{{asset("img/marchio-sito-test.png")}}" alt="logo">
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam maxime, vitae dolorum omnis doloribus eaque fugit fuga delectus, ex deleniti, velit aliquam consequuntur. Quia eos recusandae earum a, laboriosam quae?</p>
         </div>
-        <div class="footer-middle">
+        <div class="footer-middle padding-footer">
             <h4>Pastificio</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aspernatur sunt voluptatibus iusto quo autem sit ducimus officiis aliquid cumque placeat maxime error quibusdam minus, expedita vitae quas dignissimos ex.</p>
             <h4>Prodotti</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aspernatur sunt voluptatibus iusto quo autem sit ducimus officiis aliquid cumque placeat maxime error quibusdam minus, expedita vitae quas dignissimos ex.</p>
         </div>
-        <div class="footer-right">
+        <div class="footer-right padding-footer">
             <h4>Collezione da chef</h4>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aspernatur sunt voluptatibus iusto quo autem sit ducimus officiis aliquid cumque placeat maxime error quibusdam minus, expedita vitae quas dignissimos ex.</p>
         </div>
     </div>
-<img src="{{asset("img/footer-montagne-bottom.jpg")}}" alt="montagne">
+    <img class="img-footer" src="{{asset("img/footer-montagne-bottom.jpg")}}" alt="montagne">
 </footer>
